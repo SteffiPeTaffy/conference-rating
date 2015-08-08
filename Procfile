39508915ab7e9a1be3330528b328682bf5ee4aca
Stefanie Grewenig <sgreweni@thoughtworks.com>
2015-08-08 11:26:25 +0200

    initial setup

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JVM_OPTS -cp target/conference-rating.jar clojure.main -m conference-rating.server
---
+++ env/prod/cljs/conference_rating/prod.cljs
@@ -0,0 +1,7 @@
+(ns conference-rating.prod
+  (:require [conference-rating.core :as core]))
+
+;;ignore println statements in prod
+(set! *print-fn* (fn [& _]))
+
+(core/init!)
