5235a28413d0df1f8ff7fd3f5bd73aab0bc4d0a6
Florian Sellmayr <florian.sellmayr@gmail.com>
2015-08-08 17:42:25 +0200

    add support for mongolab | fs | #3

--- src/clj/conference_rating/db_handler.clj
+++ src/clj/conference_rating/db_handler.clj
@@ -3,9 +3,15 @@
                  [monger.collection :as mc])
         (:import org.bson.types.ObjectId))
 
+(def mongolab-uri (System/getenv "MONGOLAB_URI"))
+
+(defn- connect []
+  (if mongolab-uri
+    (mg/connect {:uri mongolab-uri })
+    (mg/connect {:host "127.0.0.1" :port 27017})))
 
 (defn add-conference []
-  (let [conn (mg/connect {:host "127.0.0.1" :port 27017})
+  (let [conn (connect)
         db   (mg/get-db conn "crdb")
         document { :_id (ObjectId.) :name "Devoxx" :description "Devoxx is a conference" }]
     (mc/insert db "conferences" document)
