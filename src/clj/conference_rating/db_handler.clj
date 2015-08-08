593d27272a52f6a9b26acab9bbb4e3b1d2fc9edd
Florian Sellmayr <florian.sellmayr@gmail.com>
2015-08-08 18:19:09 +0200

    connect to db specified in mongolab-uri | fs | #3

--- src/clj/conference_rating/db_handler.clj
+++ src/clj/conference_rating/db_handler.clj
@@ -7,12 +7,11 @@
 
 (defn- connect []
   (if mongolab-uri
-    (mg/connect {:uri mongolab-uri })
-    (mg/connect {:host "127.0.0.1" :port 27017})))
+    (second (mg/connect-via-uri {:uri mongolab-uri }))
+    (mg/get-db (mg/connect {:host "127.0.0.1" :port 27017}) "crdb")))
 
 (defn add-conference []
-  (let [conn (connect)
-        db   (mg/get-db conn "crdb")
+  (let [db (connect)
         document { :_id (ObjectId.) :name "Devoxx" :description "Devoxx is a conference" }]
     (mc/insert db "conferences" document)
     document))
