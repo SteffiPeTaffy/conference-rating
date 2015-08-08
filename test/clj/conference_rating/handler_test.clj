79a5721d7636db3ccb0cd79dcd8605ce0673dc93
Florian Sellmayr <florian.sellmayr@gmail.com>
2015-08-08 20:39:01 +0200

    add test for create conference| fs / cb | #3

--- test/clj/conference_rating/handler_test.clj
+++ test/clj/conference_rating/handler_test.clj
@@ -1,16 +1,15 @@
 (ns clj.conference-rating.handler-test
   (:use [conference-rating.handler])
   (:require [clojure.test :refer [deftest is testing]]
-            [ring.mock.request :refer [request]]
+            [ring.mock.request :refer [request body header]]
             [clojure.data.json :as json])
   (:import (com.github.fakemongo Fongo)))
 
 (defn json-body-for [db request] (json/read-str (:body ((app db) request)) :key-fn keyword))
 
 (defn- create-mock-db []
-  (doto
-    (Fongo. "some mock mongodb")
-    (.getDB "crdb")))
+  (let [fongo (Fongo. "some mock mongodb")]
+    (.getDB fongo "crdb")))
 
 (deftest acceptance-test
   (testing "should have an index-page"
@@ -30,10 +29,10 @@
     (is (= 200 (:status ((app (create-mock-db)) (request :get "/api/conferences")))))
     (is (= [{:conference-name "Conference 1" :id "1"}
             {:conference-name "Conference 2" :id "2"}] (json-body-for (create-mock-db) (request :get "/api/conferences")))))
-  ;(testing "should add a conference to the database"
-  ;  (let [db (create-mock-db)]
-  ;    (let [response ((app db) (request :post "/api/conferences/" {:body (json/write-str {:name "some name" :description "some description"})
-  ;                                                                 :content-type "application/json"}))]
-  ;      (is (= 201 (:status response))))
-  ;    ))
-  )
+  (testing "should add a conference to the database"
+    (let [db (create-mock-db)]
+      (let [response ((app db) (-> (request :post "/api/conferences/")
+                                   (body (json/write-str {:name "some name" :description "some description"}))
+                                   (header :content-type "application/json")))]
+        (is (= 201 (:status response)))
+        (is (.startsWith (get-in response [:headers "Location"]) "/api/conferences/"))))))
