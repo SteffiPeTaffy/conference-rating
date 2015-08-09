22235078b382f2245d8d6d695ee4f3d396718791
Stefanie Grewenig <sgreweni@thoughtworks.com>
2015-08-09 19:02:28 +0200

    prettify ome more buttons | sg | #0

--- src/cljs/conference_rating/conference/conference_list.cljs
+++ src/cljs/conference_rating/conference/conference_list.cljs
@@ -8,7 +8,7 @@
     [:p "10.09.2015 - 13.09.2015"]]
    [:div {:class "panel-body"}
     [:div {:class "text-lg-right"}
-     [:a {:class "btn btn-lg btn-primary" :href (str "#/conferences/" (:_id conference-list-entry))} "rate conference"]]
+     [:a {:class "btn btn-lg btn-primary glyphicon glyphicon-pencil" :href (str "#/conferences/" (:_id conference-list-entry))} "rate"]]
     [:p {:class "text-muted"} (:description conference-list-entry)]
     [:a {:href (str "#/conferences/" (:_id conference-list-entry))} "go to conference overview"]]])
 
@@ -16,6 +16,6 @@
   [:div {:class "container conferences-container"}
    [:h1 "Conferences"]
    [:div {:class "add-conference-container"}
-    [:a {:class "btn btn-lg btn-primary" :href "#/add-conference"} "add conference"]]
+    [:a {:class "btn btn-primary glyphicon glyphicon-plus" :href "#/add-conference"} "add conference"]]
    [:div (map display-conference-list-item conference-list)]
    [:div (panel/range-panel 55 "Okay" "Hello Banana was happy :)")]])
