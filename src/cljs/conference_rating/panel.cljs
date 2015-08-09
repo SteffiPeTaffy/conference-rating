9dcfd3a02244bdeebcb1e96f6aa3a69739b23731
stefanie <stefanie.grewenig@.googlemail.com>
2015-08-09 19:05:34 +0200

    Optimised css/less for range panel | cb | #0

--- src/cljs/conference_rating/panel.cljs
+++ src/cljs/conference_rating/panel.cljs
@@ -16,15 +16,15 @@
   (coloured-panel heading-label body "" ""))
 
 (defn range-panel [percentage name comment]
-  [:div {:class "panel panel-mint"}
-   [:div {:class "pad-all"}
+  [:div {:class "panel bg-mint"}
+   [:div {:class "media-container"}
     [:div {:class "media-left"}
-     [:div {:class "icon-wrap"}
+     [:div {:class "media-container icon-wrap"}
       [:span {:class "glyphicon glyphicon-star"}]]]
     [:div {:class "media-body"}
-     [:div {:class "h3-media-heading"} (str percentage "%")]
+     [:h3 {:class "media-body range-label"} (str percentage "%")]
      [:div {:class "text-uppercase"} name]]]
    [:div {:class "progress-xs"}
     [:div {:style {:width (str percentage "%")} :class "progessbar progressbar-light" }]]
-   [:div {:class "pad-all text-right"}
+   [:div {:class "media-container media-body text-lg-right"}
     [:span {:class "text-semi"} comment]]])
