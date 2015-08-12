326ed8a586cd39e03469817e729e44c517d7e70b
stefanie <stefanie.grewenig@.googlemail.com>
2015-08-12 23:26:38 +0200

    still prettifying the home page | sg | #0

--- src/cljs/conference_rating/conference/conference_list_entry.cljs
+++ src/cljs/conference_rating/conference/conference_list_entry.cljs
@@ -1,16 +1,23 @@
-(ns conference-rating.conference.conference-list-entry)
+(ns conference-rating.conference.conference-list-entry
+  (:require [conference-rating.panel :as panel]))
 
 (defn display-conference-list-item [conference-list-entry]
-  [:div {:key (:_id conference-list-entry) :class "col-lg-4 conference-item-container"}
+  [:div {:key (:_id conference-list-entry) :class "col-lg-6 conference-item-container"}
    [:div {:class "panel panel-heading bg-light cl-dark"}
-    [:h4 {:class "mar-no"} (:name conference-list-entry)]
-    [:p "10.09.2015 - 13.09.2015"]
+    [:div {:class "row conference-row"}
+     [:div {:class "col-md-6"}
+      [:div {:class "series-tag-container"}[:span {:class "series-tag"} "DEVOXX"]]
+      [:h4 (:name conference-list-entry)]
+      [:p "10.09.2015 - 13.09.2015"]]
+     [:div {:class "col-md-6 conference-rating-column"}
+      [:div {:class "text-lg-right"} "banana"]]]
     [:div {:class "bottom-line"}]]
    [:div {:class "panel-body  bg-light"}
     [:div {:class "row"}
-     [:div {:class "col-md-8"}
+     [:div {:class "col-md-9"}
       [:p {:class "text-muted"} (:description conference-list-entry)]
       [:a {:href (str "#/conferences/" (:_id conference-list-entry))} "go to conference overview"]]
-     [:div {:class "col-md-4"}
+     [:div {:class "col-md-3"}
+      [:div {:class "conference-overall-rating"} (panel/range-panel 83 "Overall" "12 ratings" "bg-dark-lightened" "glyphicon-thumbs-up")]
       [:div {:class "text-lg-right"}
-       [:a {:class "btn btn-sm btn-orange glyphicon glyphicon-pencil" :href (str "#/conferences/" (:_id conference-list-entry) "/add-rating")} "rate"]]]]]])
+       [:a {:class "btn btn-sm btn-orange glyphicon glyphicon-pencil voice-btn" :href (str "#/conferences/" (:_id conference-list-entry) "/add-rating")} "rate"]]]]]])
