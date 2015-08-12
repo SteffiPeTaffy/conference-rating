cdc56f9e0d2935056463c95851ea242257529d42
stefanie <stefanie.grewenig@.googlemail.com>
2015-08-12 23:35:59 +0200

    three in a row on home page and overall rating icon wrap now responsive | sg | #0

--- src/cljs/conference_rating/conference/conference_list_entry.cljs
+++ src/cljs/conference_rating/conference/conference_list_entry.cljs
@@ -2,7 +2,7 @@
   (:require [conference-rating.panel :as panel]))
 
 (defn display-conference-list-item [conference-list-entry]
-  [:div {:key (:_id conference-list-entry) :class "col-lg-6 conference-item-container"}
+  [:div {:key (:_id conference-list-entry) :class "col-lg-4 conference-item-container"}
    [:div {:class "panel panel-heading bg-light cl-dark"}
     [:div {:class "row conference-row"}
      [:div {:class "col-md-6"}
@@ -14,10 +14,10 @@
     [:div {:class "bottom-line"}]]
    [:div {:class "panel-body  bg-light"}
     [:div {:class "row"}
-     [:div {:class "col-md-9"}
+     [:div {:class "col-md-8"}
       [:p {:class "text-muted"} (:description conference-list-entry)]
       [:a {:href (str "#/conferences/" (:_id conference-list-entry))} "go to conference overview"]]
-     [:div {:class "col-md-3"}
+     [:div {:class "col-md-4"}
       [:div {:class "conference-overall-rating"} (panel/range-panel 83 "Overall" "12 ratings" "bg-dark-lightened" "glyphicon-thumbs-up")]
       [:div {:class "text-lg-right"}
        [:a {:class "btn btn-sm btn-orange glyphicon glyphicon-pencil voice-btn" :href (str "#/conferences/" (:_id conference-list-entry) "/add-rating")} "rate"]]]]]])
