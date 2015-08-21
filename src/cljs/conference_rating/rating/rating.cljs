9a184e85da1cfbbdcb04c9c57024e1c67591a99a
Stefanie Grewenig <sgreweni@thoughtworks.com>
2015-08-21 21:20:06 +0200

    shows first proper ratings from dummy data

--- src/cljs/conference_rating/rating/rating.cljs
+++ src/cljs/conference_rating/rating/rating.cljs
@@ -1,13 +1,12 @@
 (ns conference-rating.rating.rating
   (:require [conference-rating.panel :as panel]))
 
-(defn display-rating []
-  (let [rating single-rating]
-     [:div {:class "row bg-light"}
-      [:div {:class "col-lg-12 col-md-12"}
-        (if (:recommended rating) [:p "I would go again!"])
-        [:p (get-in rating [:comment :name])]
-        [:p (get-in rating [:comment :comment])]]]))
+(defn display-rating [rating]
+   [:div {:class "row bg-light"}
+    [:div {:class "col-lg-12 col-md-12"}
+      (if (:recommended rating) [:p "I would go again!"])
+      [:p (get-in rating [:comment :name])]
+      [:p (get-in rating [:comment :comment])]]])
 
 (defn display-ratings [conference-ratings]
-  [:div (map display-rating conference-ratings)])
+  [:div {:class "container-fluid pad-top"} (map display-rating conference-ratings)])
