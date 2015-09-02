66fbec9f52dd51c669668b66a1857f23dbba27bf
Stefanie Grewenig <sgreweni@thoughtworks.com>
2015-09-02 20:42:04 +0200

    filtered empty ratings and display comments semi properly

--- src/cljs/conference_rating/conference_detail_page/rating_list.cljs
+++ src/cljs/conference_rating/conference_detail_page/rating_list.cljs
@@ -1,11 +1,14 @@
 (ns conference-rating.conference-detail-page.rating-list)
 
 (defn display-rating [rating]
-   [:div {:class "row bg-light"}
-    [:div {:class "col-lg-12 col-md-12"}
-      (if (:recommended rating) [:p "I would go again!"])
-      [:p (get-in rating [:comment :name])]
-      [:p (get-in rating [:comment :comment])]]])
+   [:div {:class "rating"}
+    [:div {:class "row bg-light"}
+     [:div {:class "col-lg-12 col-md-12"}
+      [:h5 (get-in rating [:comment :name])]
+      [:p (get-in rating [:comment :comment])]]]])
+
+(defn hasComment [rating]
+  (not (and (= (get-in rating [:comment :name]) "") (= (get-in rating [:comment :comment]) ""))))
 
 (defn display-rating-list [conference-ratings]
-  [:div {:class "container-fluid pad-top"} (map display-rating conference-ratings)])
+  [:div {:class "container-fluid pad-top"} (map display-rating (filter hasComment conference-ratings))])
