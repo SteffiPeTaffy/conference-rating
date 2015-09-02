b5198e64cb78c3c74137d96827dd9514a40aad25
Stefanie Grewenig <sgreweni@thoughtworks.com>
2015-09-02 18:00:51 +0200

    removed unused fake data

--- src/cljs/conference_rating/conference_detail_page/conference_detail_page.cljs
+++ src/cljs/conference_rating/conference_detail_page/conference_detail_page.cljs
@@ -28,36 +28,6 @@
     :comment     {:name    "Florian Sellmayr"
                   :comment "This conference was mehh and awesome at the same time!"}}])
 
-(def aggregated-ratings
-  {:aggregated-ratings {:number-of-ratings 16
-                        :recommendations 1445
-                        :overall {:avg 4 :count 8}
-                        :talks {:avg 0.1 :count 7}
-                        :venue {:avg 3 :count 6}
-                        :community {:avg 2.5 :count 5}
-                        :roles {
-                                :dev 1
-                                :devops 2
-                                :ux 3
-                                :qa 4
-                                :ba 5
-                                :pm 6
-                                :sales 7
-                                :recruting 8
-                                :other 9}
-                        :experience {
-                                     :rookie 1
-                                     :beginner 3
-                                     :intermediate 12
-                                     :advanced 5
-                                     :expert 0}
-                        :tags {
-                               :inspiring 2
-                               :entertaining 2
-                               :learning 2
-                               :potential-hires 1
-                               :potential-clients 1}}})
-
 (defn add-rating-button [conference-id]
   [:div {:class "text-lg-right voice-btn-container"}
    [:a {:class "btn btn-md btn-orange" :href (str "#/conferences/" conference-id "/add-rating")} "give it your voice"]])
