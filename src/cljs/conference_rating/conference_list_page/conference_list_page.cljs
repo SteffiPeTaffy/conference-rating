ffc8cefe52f665554eac3a2264fb2da389472936
Marcos Mercuri <marcosmercuri@gmail.com>
2020-05-10 20:39:14 +0200

    Small refactor

--- src/cljs/conference_rating/conference_list_page/conference_list_page.cljs
+++ src/cljs/conference_rating/conference_list_page/conference_list_page.cljs
@@ -9,22 +9,15 @@
 (defonce displayed-past-conferences (atom nil))
 (defonce current-page-future-conferences (atom 1))
 
-(defn- turn-next-page [next-page-conferences]
+(defn- display-new-conferences [next-page-conferences]
   (reset! displayed-future-conferences next-page-conferences))
 
-(defn- load-more-conferences []
-  (swap! current-page-future-conferences inc)
-  (backend/load-future-conferences @current-page-future-conferences turn-next-page))
-
-(defn- turn-previous-page [next-page-conferences]
-  (reset! displayed-future-conferences next-page-conferences))
-
-(defn- load-less-conferences []
-  (swap! current-page-future-conferences dec)
-  (backend/load-future-conferences @current-page-future-conferences turn-previous-page))
+(defn- load-new-page [modify-atom-fn]
+  (swap! current-page-future-conferences modify-atom-fn)
+  (backend/load-future-conferences @current-page-future-conferences display-new-conferences))
 
 (defn- previous-page-handler [enable-button]
-  (if enable-button (load-less-conferences)))
+  (if enable-button (load-new-page dec)))
 
 (defn display-conference-list [future-conferences past-conferences]
   [:div {:data-e2e "page-conference-list"}
@@ -35,7 +28,7 @@
        (let [enable-button (> @current-page-future-conferences 1)]
          [:a {:class "btn btn-sm btn-orange" :data-e2e "button-previous-page-future-conferences" :on-click #(previous-page-handler enable-button) :disabled (not enable-button)} "Previous Page"])
        [:span {:class "current-page" } (str "Page: " @current-page-future-conferences)]
-       [:a {:class "btn btn-sm btn-orange" :data-e2e "button-next-page-future-conferences" :on-click #(load-more-conferences)} "Next Page"]
+       [:a {:class "btn btn-sm btn-orange" :data-e2e "button-next-page-future-conferences" :on-click #(load-new-page inc)} "Next Page"]
        ]
       [:div {:class "row"} (map #(list-entry/display-conference-list-item % future-conferences) (sort-by :to future-conferences))]
       [:h3 "Past conferences"]
