918ab9479ff8745c14d1688994267ffe6154ff9e
Marcos Mercuri <marcosmercuri@gmail.com>
2020-05-16 17:15:31 +0200

    Add test for ordered past and future conferences

--- test/clj/conference_rating/db_handler_test.clj
+++ test/clj/conference_rating/db_handler_test.clj
@@ -1,8 +1,14 @@
 (ns conference-rating.db-handler-test
   (:require [monger.collection :as mc]
             [clojure.test :refer :all]
-            [conference-rating.db-handler :as dh :refer [delete-conference-by-id get-all-conferences update-conference-by-id]]
-            [conference-rating.testdata :refer :all])
+            [conference-rating.db-handler :as dh :refer [
+                                                         delete-conference-by-id
+                                                         get-all-conferences
+                                                         update-conference-by-id
+                                                         get-future-conferences
+                                                         get-past-conferences]]
+            [conference-rating.testdata :refer :all]
+            [java-time :as jtime])
   (:import (com.github.fakemongo Fongo)
            (org.bson.types ObjectId)))
 
@@ -98,3 +104,43 @@
       (testing "that it should get a list of not deleted conferences"
         (is (= [(.toHexString id)] ids))))))
 
+(defn- insert-conference [db id today-modificator amount-modificator]
+  (let [today (jtime/local-date-time)
+        from (jtime/format :iso-date-time (today-modificator today amount-modificator))
+        to (jtime/format :iso-date-time (today-modificator today amount-modificator))]
+    (mc/insert db "conferences" {:_id id :score 1 :from from :to to})))
+
+(deftest get-future-and-past-conferences-test
+  (let [fake-db (create-mock-db)
+        id-future-1 (ObjectId.)
+        id-future-2 (ObjectId.)
+        id-future-3 (ObjectId.)
+        id-future-4 (ObjectId.)
+        id-past-1 (ObjectId.)
+        id-past-2 (ObjectId.)
+        id-past-3 (ObjectId.)]
+
+    (insert-conference fake-db id-future-1 jtime/plus (jtime/days 1))
+    (insert-conference fake-db id-future-2 jtime/plus (jtime/days 2))
+    (insert-conference fake-db id-future-3 jtime/plus (jtime/days 3))
+    (insert-conference fake-db id-future-4 jtime/plus (jtime/days 4))
+
+    (insert-conference fake-db id-past-1 jtime/minus (jtime/days 1))
+    (insert-conference fake-db id-past-2 jtime/minus (jtime/days 2))
+    (insert-conference fake-db id-past-3 jtime/minus (jtime/days 3))
+
+    (testing "should return only future conferences"
+      (let [conference-list (get-future-conferences fake-db 1 10)
+            ids (map :_id conference-list)]
+        (is (= [(.toHexString id-future-1)
+                (.toHexString id-future-2)
+                (.toHexString id-future-3)
+                (.toHexString id-future-4)]
+               ids))))
+    (testing "should return only past conferences"
+      (let [conference-list (get-past-conferences fake-db 1 10)
+            ids (map :_id conference-list)]
+        (is (= [(.toHexString id-past-1)
+                (.toHexString id-past-2)
+                (.toHexString id-past-3)]
+               ids))))))
